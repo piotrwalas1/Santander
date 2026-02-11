23fb1109db812ff5c83293a5bc9f6d0475519b8e
piotr <piotr@example.com>
2026-02-11 20:38:02 +0100

    commit2

---
+++ src/test/java/org/example/TcAsercja.java
@@ -0,0 +1,35 @@
+package Santander;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class TcAsercja {
+    @Test
+    public void account1() throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.santander.pl/klient-indywidualny");
+        String currentWindow = driver.getWindowHandle();
+        Set<String> windowsNames = driver.getWindowHandles();
+        for(String window : windowsNames){
+            if(!window.equals(currentWindow)){
+                driver.switchTo().window(window);
+            }
+        }
+        driver.findElement(By.id("privacy-prompt-controls-button-accept")).click();
+        driver.switchTo().window(currentWindow);
+
+
+
+
+    }
+
+
+
+}
